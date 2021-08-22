15781c254b66726aea368dac5fff954555994783
totoro97 <totoro97@outlook.com>
2021-08-22 21:28:20 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+# NeuS
+We present a novel neural surface reconstruction method, called NeuS, for reconstructing objects and scenes with high fidelity from 2D image inputs.
+
+![](./static/intro_1_compressed.gif)
+![](./static/intro_2_compressed.gif)
+
+## [Project page](https://lingjie0206.github.io/papers/NeuS/) |  [Paper](https://arxiv.org/abs/2106.10689) | [Data](https://drive.google.com/drive/folders/1Nlzejs4mfPuJYORLbDEUDWlc9IZIbU0C?usp=sharing)
+This is the official repo for the implementation of **NeuS: Learning Neural Implicit Surfaces by Volume Rendering for Multi-view Reconstruction**.
+
+## Usage
+
+#### Data Convention
+The data is organized as follows:
+
+```
+<case_name>
+|-- cameras_xxx.npz    # camera parameters
+|-- image
+    |-- 000.png        # target image for each view
+    |-- 001.png
+    ...
+|-- mask
+    |-- 000.png        # target mask each view (For unmasked setting, set all pixels as 255)
+    |-- 001.png
+    ...
+```
+
+Here the `cameras_xxx.npz` follows the data format in [IDR](https://github.com/lioryariv/idr/blob/main/DATA_CONVENTION.md), where `world_mat_xx` denotes the world to image projection matrix, and `scale_mat_xx` denotes the normalization matrix.
+
+### Setup
+
+Clone this repository
+
+```shell
+git clone https://github.com/Totoro97/NeuS.git
+cd NeuS
+pip install -r requirements.txt
+```
+
+<details>
+  <summary> Dependencies (click to expand) </summary>
+
+  - torch==1.8.0
+  - opencv_python==4.5.2.52
+  - trimesh==3.9.8 
+  - numpy==1.19.2
+  - pyhocon==0.3.57
+  - icecream==2.1.0
+  - tqdm==4.50.2
+  - scipy==1.7.0
+  - PyMCubes==0.1.2
+
+</details>
+
+### Running
+
+- **Training without mask**
+
+```shell
+python exp_runner.py --mode train --conf ./confs/womask.conf --case <case_name>
+```
+
+- **Training with mask**
+
+```shell
+python exp_runner.py --mode train --conf ./confs/wmask.conf --case <case_name>
+```
+
+- **Extract surface from trained model** 
+
+```shell
+python exp_runner.py --mode validate_mesh --conf <config_file> --case <case_name> --is_continue # use latest checkpoint
+```
+
+The corresponding mesh can be found in `exp/<case_name>/<exp_name>/meshes/<iter_steps>.ply`.
+
+- **View interpolation**
+
+```shell
+python exp_runner.py --mode interpolate_<img_idx_0>_<img_idx_1> --conf <config_file> --case <case_name> --is_continue # use latest checkpoint
+```
+
+The corresponding image set of view interpolation can be found in `exp/<case_name>/<exp_name>/render/`.
+
+## Citation
+
+Cite as below if you find this repository is helpful to your project:
+
+```
+@article{wang2021neus,
+  title={NeuS: Learning Neural Implicit Surfaces by Volume Rendering for Multi-view Reconstruction},
+  author={Wang, Peng and Liu, Lingjie and Liu, Yuan and Theobalt, Christian and Komura, Taku and Wang, Wenping},
+  journal={arXiv preprint arXiv:2106.10689},
+  year={2021}
+}
+```
+
+## Acknowledgement
+
+Some code snippets are borrowed from [IDR](https://github.com/lioryariv/idr) and [NeRF-pytorch](https://github.com/yenchenlin/nerf-pytorch). Thanks for these great projects.
---
+++ exp_runner.py
@@ -0,0 +1,399 @@
+import os
+import time
+import logging
+import argparse
+import numpy as np
+import cv2 as cv
+import trimesh
+import torch
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+from shutil import copyfile
+from icecream import ic
+from tqdm import tqdm
+from pyhocon import ConfigFactory
+from models.dataset import Dataset
+from models.fields import RenderingNetwork, SDFNetwork, SingleVarianceNetwork, NeRF
+from models.renderer import NeuSRenderer
+
+
+class Runner:
+    def __init__(self, conf_path, mode='train', case='CASE_NAME', is_continue=False):
+        self.device = torch.device('cuda')
+
+        # Configuration
+        self.conf_path = conf_path
+        f = open(self.conf_path)
+        conf_text = f.read()
+        conf_text = conf_text.replace('CASE_NAME', case)
+        f.close()
+
+        self.conf = ConfigFactory.parse_string(conf_text)
+        self.conf['dataset.data_dir'] = self.conf['dataset.data_dir'].replace('CASE_NAME', case)
+        self.base_exp_dir = self.conf['general.base_exp_dir']
+        os.makedirs(self.base_exp_dir, exist_ok=True)
+        self.dataset = Dataset(self.conf['dataset'])
+        self.iter_step = 0
+
+        # Training parameters
+        self.end_iter = self.conf.get_int('train.end_iter')
+        self.save_freq = self.conf.get_int('train.save_freq')
+        self.report_freq = self.conf.get_int('train.report_freq')
+        self.val_freq = self.conf.get_int('train.val_freq')
+        self.val_mesh_freq = self.conf.get_int('train.val_mesh_freq')
+        self.batch_size = self.conf.get_int('train.batch_size')
+        self.validate_resolution_level = self.conf.get_int('train.validate_resolution_level')
+        self.learning_rate = self.conf.get_float('train.learning_rate')
+        self.learning_rate_alpha = self.conf.get_float('train.learning_rate_alpha')
+        self.use_white_bkgd = self.conf.get_bool('train.use_white_bkgd')
+        self.warm_up_end = self.conf.get_float('train.warm_up_end', default=0.0)
+        self.anneal_end = self.conf.get_float('train.anneal_end', default=0.0)
+
+        # Weights
+        self.igr_weight = self.conf.get_float('train.igr_weight')
+        self.mask_weight = self.conf.get_float('train.mask_weight')
+        self.is_continue = is_continue
+        self.mode = mode
+        self.model_list = []
+        self.writer = None
+
+        # Networks
+        params_to_train = []
+        self.nerf_outside = NeRF(**self.conf['model.nerf']).to(self.device)
+        self.sdf_network = SDFNetwork(**self.conf['model.sdf_network']).to(self.device)
+        self.deviation_network = SingleVarianceNetwork(**self.conf['model.variance_network']).to(self.device)
+        self.color_network = RenderingNetwork(**self.conf['model.rendering_network']).to(self.device)
+        params_to_train += list(self.nerf_outside.parameters())
+        params_to_train += list(self.sdf_network.parameters())
+        params_to_train += list(self.deviation_network.parameters())
+        params_to_train += list(self.color_network.parameters())
+
+        self.optimizer = torch.optim.Adam(params_to_train, lr=self.learning_rate)
+
+        self.renderer = NeuSRenderer(self.nerf_outside,
+                                     self.sdf_network,
+                                     self.deviation_network,
+                                     self.color_network,
+                                     **self.conf['model.neus_renderer'])
+
+        # Load checkpoint
+        latest_model_name = None
+        if is_continue:
+            model_list_raw = os.listdir(os.path.join(self.base_exp_dir, 'checkpoints'))
+            model_list = []
+            for model_name in model_list_raw:
+                if model_name[-3:] == 'pth' and int(model_name[5:-4]) <= self.end_iter:
+                    model_list.append(model_name)
+            model_list.sort()
+            latest_model_name = model_list[-1]
+
+        if latest_model_name is not None:
+            logging.info('Find checkpoint: {}'.format(latest_model_name))
+            self.load_checkpoint(latest_model_name)
+
+        # Backup codes and configs for debug
+        if self.mode[:5] == 'train':
+            self.file_backup()
+
+    def train(self):
+        self.writer = SummaryWriter(log_dir=os.path.join(self.base_exp_dir, 'logs'))
+        self.update_learning_rate()
+        res_step = self.end_iter - self.iter_step
+        image_perm = self.get_image_perm()
+
+        for iter_i in tqdm(range(res_step)):
+            data = self.dataset.gen_random_rays_at(image_perm[self.iter_step % len(image_perm)], self.batch_size)
+
+            rays_o, rays_d, true_rgb, mask = data[:, :3], data[:, 3: 6], data[:, 6: 9], data[:, 9: 10]
+            near, far = self.dataset.near_far_from_sphere(rays_o, rays_d)
+
+            background_rgb = None
+            if self.use_white_bkgd:
+                background_rgb = torch.ones([1, 3])
+
+            if self.mask_weight > 0.0:
+                mask = (mask > 0.5).float()
+            else:
+                mask = torch.ones_like(mask)
+
+            mask_sum = mask.sum() + 1e-5
+            render_out = self.renderer.render(rays_o, rays_d, near, far,
+                                              background_rgb=background_rgb,
+                                              cos_anneal_ratio=self.get_cos_anneal_ratio())
+
+            color_fine = render_out['color_fine']
+            s_val = render_out['s_val']
+            cdf_fine = render_out['cdf_fine']
+            gradient_error = render_out['gradient_error']
+            weight_max = render_out['weight_max']
+            weight_sum = render_out['weight_sum']
+
+            # Loss
+            color_error = (color_fine - true_rgb) * mask
+            color_fine_loss = F.l1_loss(color_error, torch.zeros_like(color_error), reduction='sum') / mask_sum
+            psnr = 20.0 * torch.log10(1.0 / (((color_fine - true_rgb)**2 * mask).sum() / (mask_sum * 3.0)).sqrt())
+
+            eikonal_loss = gradient_error
+
+            mask_loss = F.binary_cross_entropy(weight_sum.clip(1e-3, 1.0 - 1e-3), mask)
+
+            loss = color_fine_loss +\
+                   eikonal_loss * self.igr_weight +\
+                   mask_loss * self.mask_weight
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            self.iter_step += 1
+
+            self.writer.add_scalar('Loss/loss', loss, self.iter_step)
+            self.writer.add_scalar('Loss/color_loss', color_fine_loss, self.iter_step)
+            self.writer.add_scalar('Loss/eikonal_loss', eikonal_loss, self.iter_step)
+            self.writer.add_scalar('Statistics/s_val', s_val.mean(), self.iter_step)
+            self.writer.add_scalar('Statistics/cdf', (cdf_fine[:, :1] * mask).sum() / mask_sum, self.iter_step)
+            self.writer.add_scalar('Statistics/weight_max', (weight_max * mask).sum() / mask_sum, self.iter_step)
+            self.writer.add_scalar('Statistics/psnr', psnr, self.iter_step)
+
+            if self.iter_step % self.report_freq == 0:
+                print(self.base_exp_dir)
+                print('iter:{:8>d} loss = {} lr={}'.format(self.iter_step, loss, self.optimizer.param_groups[0]['lr']))
+
+            if self.iter_step % self.save_freq == 0:
+                self.save_checkpoint()
+
+            if self.iter_step % self.val_freq == 0:
+                self.validate_image()
+
+            if self.iter_step % self.val_mesh_freq == 0:
+                self.validate_mesh()
+
+            self.update_learning_rate()
+
+            if self.iter_step % len(image_perm) == 0:
+                image_perm = self.get_image_perm()
+
+    def get_image_perm(self):
+        return torch.randperm(self.dataset.n_images)
+
+    def get_cos_anneal_ratio(self):
+        if self.anneal_end == 0.0:
+            return 1.0
+        else:
+            return np.min([1.0, self.iter_step / self.anneal_end])
+
+    def update_learning_rate(self):
+        if self.iter_step < self.warm_up_end:
+            learning_factor = self.iter_step / self.warm_up_end
+        else:
+            alpha = self.learning_rate_alpha
+            progress = (self.iter_step - self.warm_up_end) / (self.end_iter - self.warm_up_end)
+            learning_factor = (np.cos(np.pi * progress) + 1.0) * 0.5 * (1 - alpha) + alpha
+
+        for g in self.optimizer.param_groups:
+            g['lr'] = self.learning_rate * learning_factor
+
+    def file_backup(self):
+        dir_lis = self.conf['general.recording']
+        os.makedirs(os.path.join(self.base_exp_dir, 'recording'), exist_ok=True)
+        for dir_name in dir_lis:
+            cur_dir = os.path.join(self.base_exp_dir, 'recording', dir_name)
+            os.makedirs(cur_dir, exist_ok=True)
+            files = os.listdir(dir_name)
+            for f_name in files:
+                if f_name[-3:] == '.py':
+                    copyfile(os.path.join(dir_name, f_name), os.path.join(cur_dir, f_name))
+
+        copyfile(self.conf_path, os.path.join(self.base_exp_dir, 'recording', 'config.conf'))
+
+    def load_checkpoint(self, checkpoint_name):
+        checkpoint = torch.load(os.path.join(self.base_exp_dir, 'checkpoints', checkpoint_name), map_location=self.device)
+        self.nerf_outside.load_state_dict(checkpoint['nerf'])
+        self.sdf_network.load_state_dict(checkpoint['sdf_network_fine'])
+        self.deviation_network.load_state_dict(checkpoint['variance_network_fine'])
+        self.color_network.load_state_dict(checkpoint['color_network_fine'])
+        self.optimizer.load_state_dict(checkpoint['optimizer'])
+        self.iter_step = checkpoint['iter_step']
+
+        logging.info('End')
+
+    def save_checkpoint(self):
+        checkpoint = {
+            'nerf': self.nerf_outside.state_dict(),
+            'sdf_network_fine': self.sdf_network.state_dict(),
+            'variance_network_fine': self.deviation_network.state_dict(),
+            'color_network_fine': self.color_network.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'iter_step': self.iter_step,
+        }
+
+        os.makedirs(os.path.join(self.base_exp_dir, 'checkpoints'), exist_ok=True)
+        torch.save(checkpoint, os.path.join(self.base_exp_dir, 'checkpoints', 'ckpt_{:0>6d}.pth'.format(self.iter_step)))
+
+    def validate_image(self, idx=-1, resolution_level=-1):
+        if idx < 0:
+            idx = np.random.randint(self.dataset.n_images)
+
+        print('Validate: iter: {}, camera: {}'.format(self.iter_step, idx))
+
+        if resolution_level < 0:
+            resolution_level = self.validate_resolution_level
+        rays_o, rays_d = self.dataset.gen_rays_at(idx, resolution_level=resolution_level)
+        H, W, _ = rays_o.shape
+        rays_o = rays_o.reshape(-1, 3).split(self.batch_size)
+        rays_d = rays_d.reshape(-1, 3).split(self.batch_size)
+
+        out_rgb_fine = []
+        out_normal_fine = []
+
+        for rays_o_batch, rays_d_batch in zip(rays_o, rays_d):
+            near, far = self.dataset.near_far_from_sphere(rays_o_batch, rays_d_batch)
+            background_rgb = torch.ones([1, 3]) if self.use_white_bkgd else None
+
+            render_out = self.renderer.render(rays_o_batch,
+                                              rays_d_batch,
+                                              near,
+                                              far,
+                                              cos_anneal_ratio=self.get_cos_anneal_ratio(),
+                                              background_rgb=background_rgb)
+
+            def feasible(key): return (key in render_out) and (render_out[key] is not None)
+
+            if feasible('color_fine'):
+                out_rgb_fine.append(render_out['color_fine'].detach().cpu().numpy())
+            if feasible('gradients') and feasible('weights'):
+                n_samples = self.renderer.n_samples + self.renderer.n_importance
+                normals = render_out['gradients'] * render_out['weights'][:, :n_samples, None]
+                if feasible('inside_sphere'):
+                    normals = normals * render_out['inside_sphere'][..., None]
+                normals = normals.sum(dim=1).detach().cpu().numpy()
+                out_normal_fine.append(normals)
+            del render_out
+
+        img_fine = None
+        if len(out_rgb_fine) > 0:
+            img_fine = (np.concatenate(out_rgb_fine, axis=0).reshape([H, W, 3, -1]) * 256).clip(0, 255)
+
+        normal_img = None
+        if len(out_normal_fine) > 0:
+            normal_img = np.concatenate(out_normal_fine, axis=0)
+            rot = np.linalg.inv(self.dataset.pose_all[idx, :3, :3].detach().cpu().numpy())
+            normal_img = (np.matmul(rot[None, :, :], normal_img[:, :, None])
+                          .reshape([H, W, 3, -1]) * 128 + 128).clip(0, 255)
+
+        os.makedirs(os.path.join(self.base_exp_dir, 'validations_fine'), exist_ok=True)
+        os.makedirs(os.path.join(self.base_exp_dir, 'normals'), exist_ok=True)
+
+        for i in range(img_fine.shape[-1]):
+            if len(out_rgb_fine) > 0:
+                cv.imwrite(os.path.join(self.base_exp_dir,
+                                        'validations_fine',
+                                        '{:0>8d}_{}_{}.png'.format(self.iter_step, i, idx)),
+                           np.concatenate([img_fine[..., i],
+                                           self.dataset.image_at(idx, resolution_level=resolution_level)]))
+            if len(out_normal_fine) > 0:
+                cv.imwrite(os.path.join(self.base_exp_dir,
+                                        'normals',
+                                        '{:0>8d}_{}_{}.png'.format(self.iter_step, i, idx)),
+                           normal_img[..., i])
+
+    def render_novel_image(self, idx_0, idx_1, ratio, resolution_level):
+        """
+        Interpolate view between two cameras.
+        """
+        rays_o, rays_d = self.dataset.gen_rays_between(idx_0, idx_1, ratio, resolution_level=resolution_level)
+        H, W, _ = rays_o.shape
+        rays_o = rays_o.reshape(-1, 3).split(self.batch_size)
+        rays_d = rays_d.reshape(-1, 3).split(self.batch_size)
+
+        out_rgb_fine = []
+        for rays_o_batch, rays_d_batch in zip(rays_o, rays_d):
+            near, far = self.dataset.near_far_from_sphere(rays_o_batch, rays_d_batch)
+            background_rgb = torch.ones([1, 3]) if self.use_white_bkgd else None
+
+            render_out = self.renderer.render(rays_o_batch,
+                                              rays_d_batch,
+                                              near,
+                                              far,
+                                              cos_anneal_ratio=self.get_cos_anneal_ratio(),
+                                              background_rgb=background_rgb)
+
+            out_rgb_fine.append(render_out['color_fine'].detach().cpu().numpy())
+
+            del render_out
+
+        img_fine = (np.concatenate(out_rgb_fine, axis=0).reshape([H, W, 3]) * 256).clip(0, 255).astype(np.uint8)
+        return img_fine
+
+    def validate_mesh(self, world_space=False, resolution=64, threshold=0.0):
+        bound_min = torch.tensor(self.dataset.object_bbox_min, dtype=torch.float32)
+        bound_max = torch.tensor(self.dataset.object_bbox_max, dtype=torch.float32)
+
+        vertices, triangles =\
+            self.renderer.extract_geometry(bound_min, bound_max, resolution=resolution, threshold=threshold)
+        os.makedirs(os.path.join(self.base_exp_dir, 'meshes'), exist_ok=True)
+
+        if world_space:
+            vertices = vertices * self.dataset.scale_mats_np[0][0, 0] + self.dataset.scale_mats_np[0][:3, 3][None]
+
+        mesh = trimesh.Trimesh(vertices, triangles)
+        mesh.export(os.path.join(self.base_exp_dir, 'meshes', '{:0>8d}.ply'.format(self.iter_step)))
+
+        logging.info('End')
+
+    def interpolate_view(self, img_idx_0, img_idx_1):
+        images = []
+        n_frames = 60
+        for i in range(n_frames):
+            print(i)
+            images.append(self.render_novel_image(img_idx_0,
+                                                  img_idx_1,
+                                                  np.sin(((i / n_frames) - 0.5) * np.pi) * 0.5 + 0.5,
+                          resolution_level=4))
+        for i in range(n_frames):
+            images.append(images[n_frames - i - 1])
+
+        fourcc = cv.VideoWriter_fourcc(*'mp4v')
+        video_dir = os.path.join(self.base_exp_dir, 'render')
+        os.makedirs(video_dir, exist_ok=True)
+        h, w, _ = images[0].shape
+        writer = cv.VideoWriter(os.path.join(video_dir,
+                                             '{:0>8d}_{}_{}.mp4'.format(self.iter_step, img_idx_0, img_idx_1)),
+                                fourcc, 30, (w, h))
+
+        for image in images:
+            writer.write(image)
+
+        writer.release()
+
+
+if __name__ == '__main__':
+    print('Hello Wooden')
+
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+    FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--conf', type=str, default='./confs/base.conf')
+    parser.add_argument('--mode', type=str, default='train')
+    parser.add_argument('--mcube_threshold', type=float, default=0.0)
+    parser.add_argument('--is_continue', default=False, action="store_true")
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--case', type=str, default='')
+
+    args = parser.parse_args()
+
+    torch.cuda.set_device(args.gpu)
+    runner = Runner(args.conf, args.mode, args.case, args.is_continue)
+
+    if args.mode == 'train':
+        runner.train()
+    elif args.mode == 'validate_mesh':
+        runner.validate_mesh(world_space=True, resolution=512, threshold=args.mcube_threshold)
+    elif args.mode.startswith('interpolate'):  # Interpolate views given two image indices
+        _, img_idx_0, img_idx_1 = args.mode.split('_')
+        img_idx_0 = int(img_idx_0)
+        img_idx_1 = int(img_idx_1)
+        runner.interpolate_view(img_idx_0, img_idx_1)
